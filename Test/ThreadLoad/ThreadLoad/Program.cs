57be3285ac0b68ccbf2ccb67611eae945da2c83b
WangTian <153309601@qq.com>
2013-11-17 08:35:41 +0800

    add a demo load matchinfo by thread
    添加了一个使用多线程读取比赛信息的例子

---
+++ Test/ThreadLoad/ThreadLoad/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace ThreadLoad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo directory = new DirectoryInfo(Globe.RootDir);
+            DirectoryInfo[] leagueDir = directory.GetDirectories();
+
+            foreach (DirectoryInfo league in leagueDir)
+            {
+                LoadMatch loadMatch = new LoadMatch(league.Name, league.FullName);
+                Thread leagueThread = new Thread(new ThreadStart(loadMatch.LoadFolder));
+                leagueThread.Start();
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class LoadMatch
+    {
+        private string LeagueName;
+        private string LeagueDir;
+
+        public LoadMatch(string leagueName, string leagueDir)
+        {
+            this.LeagueName = leagueName;
+            this.LeagueDir = leagueDir;
+        }
+
+        public void LoadFolder()
+        {
+            DirectoryInfo directory = new DirectoryInfo(LeagueDir);
+            FileInfo[] fileInfos = directory.GetFiles();
+
+            if (fileInfos.Length > 0)
+            {
+                Console.WriteLine(LeagueName + ": Start...");
+
+                foreach (FileInfo file in fileInfos)
+                {
+                    if (file.Length < Globe.IncorrectFileSize)
+                        continue;
+
+                    string fileName = Path.GetFileNameWithoutExtension(file.FullName);
+
+                    if (fileName.Equals("LiveScores"))
+                        continue;
+
+                    string htmlContent = LoadFile(file.FullName);
+                    Thread.Sleep(Globe.loadFileTime);
+                    Console.WriteLine(LeagueName + ": Match " + fileName + " Loading complete!");
+                }
+
+                Console.WriteLine(LeagueName + ": Complete!");
+            }
+        }
+
+        private static string LoadFile(string fileName)
+        {
+            string htmlContent = "";
+
+            try
+            {
+                StreamReader sr = new StreamReader(fileName, Encoding.Default);
+                htmlContent = sr.ReadToEnd();
+            }
+            catch (Exception) { }
+
+            return htmlContent;
+        }
+    }
+}
